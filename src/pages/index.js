f6263fdfadc0c59cc24e54ca42268b549fc33098
sonford <sonfordonyango@gmail.com>
2018-08-30 23:27:24 +0300

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>My Name Is Sonford Onyango</p>
-    <p>I am a web developer (Elixir Phoenix Framework, PHP Laravel Framework.</p>
+    <p>I am a web developer (Elixir Phoenix Framework, PHP Laravel Framework).</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
